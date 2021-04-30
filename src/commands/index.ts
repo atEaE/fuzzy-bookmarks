cb8f33d7e13d89c77a4ac83900458f26e16c9a06
@EaE <30979044+atEaE@users.noreply.github.com>
2021-04-30 23:22:32 +0900

    fix: #33 multi selected remove. (#34)
    * fix: #29 impl export command. (#30)
    
    * fix: #33
    
    * fix: lint

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,5 @@
 export { showExecute } from './show';
 export { registerExecute } from './register';
 export { removeExecute } from './remove';
+export { exportExecute } from './export';
 export { setupExecute } from './setup';
